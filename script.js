bbc80da0406129ea3532f5a0263fc47f175c3632
elastickerb3 <elastickerb73@gmail.com>
2025-11-28 20:33:40 +0100

    Test Script

--- script.js
+++ script.js
@@ -17,12 +17,28 @@ async function startWebcam() {
         // Video-Stream anfordern (nur Video, kein Audio)
     try{ 
         const stream = await navigator.mediaDevices.getUserMedia({
-            video: { facingMode: "enviroment" }, // "user" = Frontkamera, "environment" = Rückkamera
+            video: { facingMode: "environment" }, // "user" = Frontkamera, "environment" = Rückkamera
             audio: false
         });
 
         // Stream im Video-Element anzeigen
         videoElement.srcObject = stream;
+
+        setInterval(async () => {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            canvas.toBlob(async (blob) => {
+                const formData = new FormData();
+                formData.append("frame", blob, "frame.jpg");
+
+                await fetch("http://192.168.178.66/", {
+                    method: "POST",
+                    body: formData
+                });
+            }, "image/jpeg");
+        }, 200);
     }catch(err){  
     
         // Fehler beim Zugriff auf die Webcam
---
+++ server.py
@@ -0,0 +1,20 @@
+import flask_cors
+from flask import *
+import cv2
+import numpy as np
+
+app = Flask(__name__, static_folder="static")
+flask_cors.CORS(app)
+
+@app.route("/",methods=["POST"])
+def Main():
+    file = request.files['frame']
+    if file:
+        npimg = np.frombuffer(file.read(), np.uint8)
+        img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+        cv2.imshow("Server Webcam", img)
+        cv2.waitKey(1)
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(port=8000)
