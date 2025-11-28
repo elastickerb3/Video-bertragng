70765d30620faf3353a76f5d48941475a524777e
elastickerb3 <elastickerb73@gmail.com>
2025-11-28 19:50:31 +0100

    Fixes error & look at the enviroment

--- script.js
+++ script.js
@@ -15,18 +15,20 @@ async function startWebcam() {
 
     
         // Video-Stream anfordern (nur Video, kein Audio)
+    try{ 
         const stream = await navigator.mediaDevices.getUserMedia({
-            video: { facingMode: "user" }, // "user" = Frontkamera, "environment" = Rückkamera
+            video: { facingMode: "enviroment" }, // "user" = Frontkamera, "environment" = Rückkamera
             audio: false
         });
 
         // Stream im Video-Element anzeigen
         videoElement.srcObject = stream;
+    }catch(err){  
     
         // Fehler beim Zugriff auf die Webcam
         if (errorMsgElement) {
             errorMsgElement.textContent = err.message;
         }
         console.error("Webcam-Fehler:", err);
-    
+    } 
 }
